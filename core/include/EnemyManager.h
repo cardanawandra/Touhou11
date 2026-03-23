0134a05596838db5214adc253c86bbb9b1f2ef44
littolani <sparkra1n@proton.me>
2026-03-22 20:33:47 -0700

    Add EnemyManager

--- core/include/EnemyManager.h
+++ core/include/EnemyManager.h
@@ -1,9 +1,27 @@
 #pragma once
-
+#include "AnmLoaded.h"
+#include "Chain.h"
 #include "Chireiden.h"
+#include "EclManager.h"
 #include "Macros.h"
+#include "Timer.h"
+
 
 class EnemyManager
 {
-
+public:
+    uint32_t flags;
+    uint32_t idk0;
+    ChainElem* chain1;
+    ChainElem* onDraw;
+    int idk1[12];
+    AnmLoaded* bulletAnm;
+    AnmLoaded* anmLoadeds[3];
+    Timer timer;
+    EclManager* enemyController;
+    int someStruct2;
+    int idk3;
+    int someIndicator;
+    int idk4;
 };
+ASSERT_SIZE(EnemyManager, 0x78);
