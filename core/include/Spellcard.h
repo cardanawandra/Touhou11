307833b7337f38508246e6a58c04d5668845e214
littolani <sparkra1n@proton.me>
2026-04-03 23:55:52 -0700

    Fill in spellcard structure

--- core/include/Spellcard.h
+++ core/include/Spellcard.h
@@ -1,11 +1,39 @@
 #pragma once
-
+#include "AnmId.h"
+#include "AnmVm.h"
+#include "Chain.h"
 #include "Chireiden.h"
 #include "Macros.h"
+#include "Timer.h"
+#include "Vectors.h"
 
+#pragma pack(push, 1)
 class Spellcard
 {
 public:
-
-private:
+    int unk0[2];
+    ChainElem* chainElem0;
+    ChainElem* chainElem1;
+    AnmVm dualVms[2];
+    AnmId anmId1;
+    AnmId anmId2;
+    AnmId anmId3;
+    AnmId anmId4;
+    Timer timer_0x888;
+    int unk1[16];
+    int someValue0;
+    uint32_t flags;
+    int someInt;
+    int someOtherInt;
+    int someInt2;
+    int unk2;
+    int someCounter;
+    int unk3;
+    double timeOffset;
+    double deltaTime;
+    int unk4;
+    Float3 someVec3;
+    int unk5[2];
 };
+#pragma pack(pop)
+ASSERT_SIZE(Spellcard, 0x924);
